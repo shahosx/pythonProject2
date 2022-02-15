7593d7b258cc0deea47f55465443a54d59a60e3a
Shahadat Hossain <shahosx@gmail.com>
2022-02-15 02:02:48 -0500

    1

--- solution.py
+++ solution.py
@@ -1,3 +1,4 @@
+#import socket
 from socket import *
 # In order to terminate the program
 import sys
@@ -6,35 +7,37 @@ import sys
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
     # Prepare a server socket
-    serverSocket.bind(("", port))
+    serverSocket.bind(('', port))
     # Fill in start
-
+    serverSocket.listen(2)
     # Fill in end
 
     while True:
         # Establish the connection
-        # print('Ready to serve...')
-        connectionSocket, addr =  # Fill in start      #Fill in end
+        connectionSocket, addr = serverSocket.accept()# Fill in start      #Fill in end
         try:
 
             try:
-                message =  # Fill in start    #Fill in end
+                message = serverSocket.recv(1024).decode() # Fill in start    #Fill in end
                 filename = message.split()[1]
                 f = open(filename[1:])
-                outputdata =  # Fill in start     #Fill in end
+                outputdata = f.read()# Fill in start     #Fill in end
 
                 # Send one HTTP header line into socket.
                 # Fill in start
 
                 # Fill in end
-
+                connectionSocket.send(outputdata.encode())
                 # Send the content of the requested file to the client
                 for i in range(0, len(outputdata)):
                     connectionSocket.send(outputdata[i].encode())
 
-                connectionSocket.send("\r\n".encode())
+                connectionSocket.send('\r\n'.encode())
                 connectionSocket.close()
             except IOError:
+                connectionSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())
+                connectionSocket.send('<html><head></head><body><h1> 404 Not Found</h1></body></html>\r\n')
+
         # Send response message for file not found (404)
         # Fill in start
 
