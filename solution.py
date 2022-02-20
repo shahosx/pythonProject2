5c9a148780ae5993c43508d2527171d793f0bd65
Shahadat Hossain <shahosx@gmail.com>
2022-02-20 01:02:52 -0500

    smtp1

--- solution.py
+++ solution.py
@@ -1,14 +1,17 @@
+import socket
 from socket import *
+import smtplib
 
-
-def smtp_client(port=1025, mailserver='127.0.0.1'):
-    msg = "\r\n My message"
+def smtp_client(port=1025, mailserver='smtp@gmail.com'):
+    msg = "\r\n My message email from Shahadat Hossain"
     endmsg = "\r\n.\r\n"
-
+    host =''
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-
+    gmail_user= 'shahosx@gmail.com'
+    gmail_Password = ''
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
-
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    clientSocket.bind('',port)
     # Fill in start
     # Fill in end
 
@@ -22,17 +25,21 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
     #print(recv1)
-    #if recv1[:3] != '250':
-    #    print('250 reply not received from server.')
+    if recv1[:3] != '250':
+        print('250 reply not received from server.')
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
+    sent_from =gmail_user
+    FROM =[shahosx@gmail.com]
     # Fill in end
-
+    Subject = ''
     # Send RCPT TO command and handle server response.
     # Fill in start
-    # Fill in end
+    send_To = 'shahadatx@gmail.com'
 
+    # Fill in end
+    clientSocket.send(msg.encode())
     # Send DATA command and handle server response.
     # Fill in start
     # Fill in end
@@ -43,6 +50,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
+    clientSocket.send(endmsg.encode())
     # Fill in end
 
     # Send QUIT command and handle server response.
